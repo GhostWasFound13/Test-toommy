c83ae1656ea3f7385e82420c73c3cd387aa20f03
SHadowYTXDLoL <87599595+Ghostnothere@users.noreply.github.com>
2022-10-06 08:33:03 +0000

    Update changed new music

--- doggo.js
+++ doggo.js
@@ -1,15 +1,214 @@
-const Aoijs = require('aoi.js');
+const Aoijs = require("aoi.js")
+ 
+const bot = new Aoijs.Bot({
+token: process.env.tommy, 
+prefix: "dead", //<- Change whatever with your prefix//
+intents: "all",
+mobilePlatform: true,
+database: {
+    db: require("dbdjs.db"),
+    type: "dbdjs.db",
+    path: "./database/",
+    tables: ["main"]
+  }
+})
+
+bot.status({
+  text: "Music",
+  type: "LISTENING",
+  time: 3
+})
+
+//Callbacks
+//bot.onMessage()
+bot.onInteractionCreate()
+
+bot.readyCommand({
+ channel: "$getVar[channelstatus]",
+ $if: "v4",
+ code: `$if[$getVar[channelstatus]==]
+$log[$userTag[$clientID] active at $formatDate[$dateStamp;LLLL]]
+$else
+$sendMessage[<@$clientID> active at <t:$cropText[$dateStamp;10]:F>;no]
+$endif
+$log[$replaceText[$replaceText[$checkContains[$get[update];up to date];true;No update found.];false;$get[update]
+____________________________________________
+Require Restart to Update Package.]]
+$let[update;$exec[npm i https://github.com/akaruidevelopment/music#main]]
+$log[Checking Music Package..]
+$log[____________________________________________
+YouTube    : $get[youtube]
+SoundCloud : $get[soundcloud]
+Spotify    : $get[spotify]
+____________________________________________
+Load       : $numberSeparator[$divide[$sub[$dateStamp;$get[time]];1000]]s
+____________________________________________]
+$let[youtube;$replaceText[$replaceText[$isValidLink[https://youtube.com/];true;✅];false;❌]]
+$let[soundcloud;$replaceText[$replaceText[$isValidLink[https://soundcloud.com/];true;✅];false;❌]]
+$let[spotify;$replaceText[$replaceText[$isValidLink[https://spotify.com/];true;✅];false;❌]]
+$resetServerVar[buttonmusichannel]
+$resetServerVar[buttonmusicmessage]
+$resetServerVar[filters]
+$let[time;$dateStamp]
+$suppressErrors`
+})
+
+const voice = new Aoijs.Voice(bot, {
+  cache: {
+    cacheType: "Memory", //Disk | None | Memory
+    directory: "./music/", //Only for "Disk"
+    enabled: true
+  },
+  soundcloud: {
+    clientId: "your clientid",
+    likeTrackLimit: 200
+  },
+  playerOptions: {
+ trackInfoInterval: 150
+},
+}); 
+
+voice.onTrackStart()
+
+voice.trackStartCommand({
+ channel: "$channelID",
+ $if: "v4",
+ code: `$if[$getServerVar[maxvol]<=$volume]
+$volume[$getServerVar[maxvol]]
+$endif
+$if[$hasPerms[$guildID;$clientID;deafenmembers]-$getVar[deafenclient]==true-1]
+$deafenUser[$clientID;yes]
+$endif
+$if[$getServerVar[buttonmusic]==0]
+$author[1;Started Playing;$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com];true;$getVar[ytemoji]];false;$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com];true;$getVar[scemoji]];false;$getVar[customemoji1]]]] 
+$title[1;$songInfo[title];$songInfo[url]]
+$addField[1;Filters;\`$getServerVar[filters]\`;no]
+$addField[1;Loop;\`$replaceText[$replaceText[$checkCondition[$loopStatus==none];true;off];false;on - $loopStatus]\`;yes]
+$addField[1;24/7;$replaceText[$replaceText[$getGlobalUserVar[247;$songInfo[user.id]];0;\`❌\`];1;\`✅\`];yes]
+$addField[1;Song;\`$numberSeparator[$queueLength]\`;yes]
+$addfield[1;Create;$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com];true;<t:$cropText[$songInfo[createdTimestamp];10]:d>];false;\`none\`];yes] 
+$addField[1;Likes;\`$numberSeparator[$replaceText[$songInfo[likes];null;0]]\`;yes]
+$addField[1;$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com;open.spotify.com];true;Listened];false;Views];\`$numberSeparator[$replaceText[$songInfo[views];null;0]]\`;yes] 
+$addField[1;Platform;\`$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com];true;YouTube];false;$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com];true;SoundCloud];false;$replaceText[$replaceText[$checkContains[$songInfo[url];open.spotify.com];true;Spotify];false;Audio]]]\`;yes]
+$addField[1;Artist;\`$songInfo[author]\`;yes] $addField[1;Duration;\`$replaceText[$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com;open.spotify.com];false;$humanizeMS[$songInfo[duration];4]];true;$djsEval[new Date($replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com;open.spotify.com];false;0];true;$findNumbers[$songInfo[duration]]]).toISOString().substr(11, 8);yes]];00:00:00;LIVE]\`;yes]
+$addField[1;Requested By;<@$songInfo[user.id]>;no]
+$addTimestamp[1;$dateStamp] 
+$thumbnail[1;$replaceText[$songInfo[thumbnail];undefined;$userAvatar[$clientID;1024]]]
+$color[1;$getVar[color]]
+$onlyIf[$checkCondition[$getServerVar[logmusic]==1]==true;]
+$else
+$if[$getServerVar[buttonmusicmessage]==]
+$loop[1;{};autocontrolmusic]
+$setServerVar[buttonmusichannel;$channelID]
+$setServerVar[buttonmusicmessage;$get[id]]
+$let[id;$sendMessage[{"embeds": "{newEmbed:{author:Started Playing:$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com];true;$getVar[ytemoji]];false;$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com];true;$getVar[scemoji]];false;$getVar[customemoji1]]]} {title:$replaceText[$songInfo[title];";']} {url:$songInfo[url]} {field:Requested By:<@$songInfo[user.id]>:no} {field:Duration:\`$replaceText[$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com;open.spotify.com];false;$humanizeMS[$songInfo[duration];4]];true;$djsEval[new Date($replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com;open.spotify.com];false;0];true;$findNumbers[$songInfo[duration]]]).toISOString().substr(11, 8);yes]];00:00:00;LIVE]\`$replaceText[$replaceText[$checkCondition[$getServerVar[buttonmusicmessage]==];true;];false; - \`$replaceText[$replaceText[$checkCondition[$songInfo[duration]==0];true;LIVE];false;$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com;open.spotify.com];false;$humanizeMS[$getCurrentDuration;4]];true;$djsEval[new Date($replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com;open.spotify.com];false;0];true;$getCurrentDuration]).toISOString().substr(11, 8);yes]]]\`]:yes} {field:Artist:\`$songInfo[author]\`:yes} {field:Platform:\`$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com];true;YouTube];false;$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com];true;SoundCloud];false;$replaceText[$replaceText[$checkContains[$songInfo[url];open.spotify.com];true;Spotify];false;Audio]]]\`:yes} {field:$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com;open.spotify.com];true;Listened];false;Views]:\`$numberSeparator[$replaceText[$songInfo[views];null;0]]\`:yes} {field:Likes:\`$numberSeparator[$replaceText[$songInfo[likes];null;0]]\`:yes} {field:Create:$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com];true;<t:$cropText[$songInfo[createdTimestamp];10]:d>];false;\`none\`]:yes} {field:Song:\`$numberSeparator[$queueLength]\`:yes} {field:24/7:$replaceText[$replaceText[$getGlobalUserVar[247;$songInfo[user.id]];0;\`❌\`];1;\`✅\`]:yes} {field:Loop:\`$replaceText[$replaceText[$checkCondition[$loopStatus==none];true;off];false;on - $loopStatus]\`:yes} {field:Filters:\`$getServerVar[filters]\`:no} {timestamp} {thumbnail:$replaceText[$songInfo[thumbnail];undefined;$userAvatar[$clientID;1024]]} {color:$getVar[color]}}", "components": "{actionRow:{button::1:queue:no:⏏} {button::3:previous:no:⏮} {button::3:play:no:⏯} {button::3:next:no:⏭} {button::1:stop:no:⏹}} {actionRow:{button:$replaceText[$replaceText[$checkCondition[$loopStatus==none];true;Off];false;$toLocaleUppercase[$loopStatus]]:1:loop:no:🔁} {button:-10s:2:downseek:no:⏪} {button:+10s:2:fastseek:no:⏩} {button::1:shuffle:no:🔀}} {actionRow:{button:$truncate[$volume]%:1:volmute:no:🔈} {button:-10%:2:voldown:no:🔉} {button:+10%:2:volup:no:🔊}}" };yes]]
+$else
+$editMessage[$getServerVar[buttonmusicmessage];{newEmbed:{author:Started Playing:$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com];true;$getVar[ytemoji]];false;$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com];true;$getVar[scemoji]];false;$getVar[customemoji1]]]}
+{title:$songInfo[title]}
+{url:$songInfo[url]}
+{field:Requested By:<@$songInfo[user.id]>:no}
+{field:Duration:\`$replaceText[$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com;open.spotify.com];false;$humanizeMS[$songInfo[duration];4]];true;$djsEval[new Date($replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com;open.spotify.com];false;0];true;$findNumbers[$songInfo[duration]]]).toISOString().substr(11, 8);yes]];00:00:00;LIVE]\` - \`$replaceText[$replaceText[$checkCondition[$songInfo[duration]==0];true;LIVE];false;$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com;open.spotify.com];false;$replaceText[$replaceText[$checkCondition[$getCurrentDuration<1000];true;0s];false;$humanizeMS[$getCurrentDuration;4]]];true;$djsEval[new Date($replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com;soundcloud.com;open.spotify.com];false;0];true;$getCurrentDuration]).toISOString().substr(11, 8);yes]]]\`:yes}
+{field:Artist:\`$songInfo[author]\`:yes}
+{field:Platform:\`$replaceText[$replaceText[$checkContains[$songInfo[url];youtube.com];true;YouTube];false;$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com];true;SoundCloud];false;$replaceText[$replaceText[$checkContains[$songInfo[url];open.spotify.com];true;Spotify];false;Audio]]]\`:yes}
+{field:$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com;open.spotify.com];true;Listened];false;Views]:\`$numberSeparator[$replaceText[$songInfo[views];null;0]]\`:yes}
+{field:Likes:\`$numberSeparator[$replaceText[$songInfo[likes];null;0]]\`:yes}
+{field:Create:$replaceText[$replaceText[$checkContains[$songInfo[url];soundcloud.com];true;<t:$cropText[$songInfo[createdTimestamp];10]:d>];false;\`none\`]:yes}
+{field:Song:\`$numberSeparator[$queueLength]\`:yes}
+{field:24/7:$replaceText[$replaceText[$getGlobalUserVar[247;$songInfo[user.id]];0;\`❌\`];1;\`✅\`]:yes}
+{field:Loop:\`$replaceText[$replaceText[$checkCondition[$loopStatus==none];true;off];false;on - $loopStatus]\`:yes}
+{field:Filters:\`$getServerVar[filters]\`:no}
+{timestamp}
+{thumbnail:$replaceText[$songInfo[thumbnail];undefined;$userAvatar[$clientID;1024]]}
+{color:$getVar[color]}}]
+$endif
+$endif
+$setGlobalUserVar[cacheplay;$songInfo[url];$songInfo[user.id]]
+$setGlobalUserVar[listenuser;$sum[$getGlobalUserVar[listenuser;$songInfo[user.id]];1];$songInfo[user.id]]
+$setServerVar[listenserver;$sum[$getServerVar[listenserver];1]]
+$setVar[listenglobal;$sum[$getVar[listenglobal];1]]
+$onlyIf[$getCurrentDuration<1;]
+$setServerVar[ratetime;$sum[$dateStamp;$getVar[customratetime]]]
+$playerConfig[$replaceText[$replaceText[$getGlobalUserVar[247;$songInfo[user.id]];0;yes];1;no];0s;yes]
+$volume[$getGlobalUserVar[vol;$songInfo[user.id]]]
+$suppressErrors
+$onlyIf[$getServerVar[ratetime]<$dateStamp;{execute:forcestop}]`
+})
+
+voice.trackStartCommand({
+ channel: "$channelID",
+ $if: "v4",
+ code: `$setGlobalUserVar[playlistusercount;$sum[$getGlobalUserVar[playlistusercount;$songInfo[user.id]];1];$songInfo[user.id]]
+$setGlobalUserVar[playlistuser;$getGlobalUserVar[playlistuser;$songInfo[user.id]],
+"name$getGlobalUserVar[playlistusercount;$songInfo[user.id]]": "[$replaceText[$replaceText[$songInfo[title];";'];:;#COLON#]]($replaceText[$songInfo[url];:;#COLON#])";$songInfo[user.id]]
+$if[$getGlobalUserVar[playlistuserbypass;$songInfo[user.id]]$suppressErrors==0]
+$onlyIf[$checkContains[$songInfo[url];youtube.com;soundcloud.com;cdn.discordapp.com;open.spotify.com]==true;]
+$onlyIf[$checkContains[$getGlobalUserVar[playlistuser;$songInfo[user.id]];$songInfo[url]]==false;]
+$endif
+$onlyIf[$getGlobalUserVar[playlistuserauto;$songInfo[user.id]]!=off;]
+$suppressErrors`
+})
+
+bot.awaitedCommand({
+ name: "forcestop",
+ $if: "v4",
+ code: `$if[$getGlobalUserVar[247;$songInfo[user.id]]$suppressErrors==0]
+$log[Rate Limited Detected, when playing song at $formatDate[$dateStamp;LLLL]]
+$sendMessage[{newEmbed:{title:Rate Limited Detected} {description:Force stop active.} {color:ff0000} {timestamp}};no]
+$leaveVC
+$else
+$log[Rate Limited Detected, when playing song at $formatDate[$dateStamp;LLLL]]
+$sendMessage[{newEmbed:{title:Rate Limited Detected} {description:Force stop active.} {color:ff0000} {timestamp}};no]
+$stop
+$endif
+$suppressErrors`
+})
+
+voice.onTrackEnd()
+
+voice.trackEndCommand({
+ channel: "$channelID",
+ $if: "v4",
+ code: `$if[$getServerVar[buttonmusic]==1]
+$deleteMessage[$get[musicmessage];$get[musichannel]]
+$endif
+$setServerVar[buttonmusichannel;]
+$setServerVar[buttonmusicmessage;]
+$let[musichannel;$getServerVar[buttonmusichannel]]
+$let[musicmessage;$getServerVar[buttonmusicmessage]]
+$setServerVar[filters;$getVar[filters]]
+$onlyIf[$hasPlayer!=true;]
+$suppressErrors`
+})
+
+voice.onQueueEnd()
+
+voice.queueEndCommand({
+ channel: "$channelID",
+ $if: "v4",
+ code: `$setServerVar[buttonmusichannel;]
+$setServerVar[buttonmusicmessage;]
+$setServerVar[filters;$getVar[filters]]
+$if[$getServerVar[buttonmusic]==1]
+$deleteMessage[$get[musicmessage];$get[musichannel]]
+$let[musichannel;$getServerVar[buttonmusichannel]]
+$let[musicmessage;$getServerVar[buttonmusicmessage]]
+$endif
+$suppressErrors`
+})
+
+
+/*const Aoijs = require('aoi.js');
 const fs = require('fs');
 const { Bot,LoadCommands,Voice } = require('aoi.js');
 const config = require('./config.js');
 const bot = new Aoijs.Bot(config.Bot);
-
-//Distube for music has been setup
-const filters = require('./filters.json')
-const { DisTube } = require("distube");
-const { SpotifyPlugin } = require("@distube/spotify");
-const spotifyoptions = {
-  parallel: true,
+/* ... */
+//Distube for music has been setup**
+//const filters = require('./filters.json')
+//const { DisTube } = require("distube");
+//const { SpotifyPlugin } = require("@distube/spotify");
+//const spotifyoptions = {
+ /* parallel: true,
   emitEventsAfterFetching: true,
   api: {
     clientId: 'bf5ee2a72bae40ffadc71a47280e5ff9', // Spotify ClientID
@@ -29,22 +228,22 @@ emitNewSongOnly: false,
     customFilters: filters,
   plugins: [new SpotifyPlugin(spotifyoptions)]
 }) 
+/* ... */
+//const variables = require("./variables")
 
-const variables = require("./variables")
+//bot.variables(variables)
+//*
+//require('./handler/DistubeEvents')(bot)
+//require('./handler/status')(bot)
 
-bot.variables(variables)
-
-require('./handler/DistubeEvents')(bot)
-require('./handler/status')(bot)
-
-require('./handler/functions')(bot)
+//require('./handler/functions')(bot)
 //require("./handler/pathToHandlerFile.js"); not working
 //onst loadFunction = require("pathToHandlerFile");
 
-/* ... */
 
 //loadFunction(bot, "pathToFunctionDir"); 
 // :/
+bot.variables(require(`./variables/variables.js`))
 
 bot.onJoin()
 bot.onMessage()
@@ -80,26 +279,26 @@ app.listen(3000, () => {
 
 
 
-//events
-const files = fs.readdirSync('./events').filter(file => file.endsWith('.js'))
-files.forEach( x => {
-require(`./events/${x}`)(bot)
-});{
-}
+//events.//*
+//*const files = fs.readdirSync('./events').filter(file => file.endsWith('.js'))
+//files.forEach( x => {
+//require(`./events/${x}`)(bot)
+//});{
+//}
 
 
-const voice = new Voice(bot);
+//const voice = new Voice(bot);
 
 ////Handlers™
-Handler({ bot,loader,voice });
+//Handler({ bot,loader,voice });
 
-const Lavalink = new aoijs.Lavalink(bot);
-Lavalink.addNode({
- url: "lava.link:80",
- password: "neo",
- name: "aoi.js",
- secure: true,
- })
+//const Lavalink = new aoijs.Lavalink(bot);
+//Lavalink.addNode(
+//url: "lava.link:80",
+ //password: "neo",
+ //name: "aoi.js",
+ //secure: true,
+// })
 
   //BOT JOIN lEAVE SERVER LOGGER
 
--- variables.js
+++ variables.js
@@ -1,4 +1,81 @@
 module.exports = {
+  
+  //Customize Property For Message
+  pause: "⏸️ Paused.",
+  resume: "▶️ Resumed!",
+  skip: "⏩ Skipped!",  //Available {song}
+  stop: "⏹ Stopped.",  
+  remove: "Removed song on {d-amount}.", //Available {d-amount}
+  shuffle: "Shuffle Queue.",
+
+  //Error Message
+  errorjoin: "\`❌ You're not in a voice channel.\`",
+  errorqueue: "\`❌ There no song was playing.\`",
+  errorloop: "Only have {amount} song.", //Available {amount}
+  erroruser: "{newEmbed:{title:❌ You cant use this command} {color:$getVar[color]}}",
+  errorsameuser: "You must same in {voice} to use this command.", //Available {voice}
+  errorbutton: "You cant use this.",
+  customerror: "Something just happened.", //Custom $suppressErrors
+
+  //Event Message
+  join: "Joined Voice Channel to the {join}.", //Available {join}
+  dc: "Disconnected.",
+  leftvc: "There no song again on queue.", //Description
+  secondleftvc: "Left VC.", //Footer
+
+  //Changing Other
+  color: "a09fff",
+  channelstatus: "", //Optional (channelid), for send ready message
+  vol: "50", //Default Volume
+  maxvol: "150", //Max Volume
+
+  //Changing Other - Advance
+  permission: "277070867456",
+  deafenclient: "1", //Server Deafen Client, 0 = false | 1 = true
+  defaultspotify: "youtube", //YouTube/SoundCloud
+  multiseek: "1000", //Seek Command
+  userid: "default",
+  logmusic: "1", //0 = off | 1 = on
+  247: "0", //0 = off | 1 = stay 24/7
+  last: "null",
+  linkdownload: "", //Stream HLS (Soon)
+  filters: "none",
+  cachemessageid: "",
+  cacheplay: "",
+  ratetime: "0",
+  customratetime: "300",
+  listfilters: "\`bassonly, clarity, deep, echo, flanger, gate, haas, nightcore, phaser, phone, pitch, pulsator, reverb, tempo, tremolo, remove, subboost, vaporwave, vibrato\`",
+
+  //Emoji(s)
+  customemoji1: "https://cdn.discordapp.com/emojis/852434440668184615.png?size=4096",
+  customemoji2: "https://cdn.discordapp.com/emojis/951749233919279125.png?size=4096",
+  ytemoji: "https://cdn.discordapp.com/emojis/852432148207108110.png?size=4096",
+  scemoji: "https://cdn.discordapp.com/emojis/852432173758676993.png?size=4096",
+  loademoji: "https://cdn.discordapp.com/emojis/951749045787959337.gif?size=4096",
+ 
+  //Listen-Info
+  listenuser: "0",
+  listenserver: "0",
+  listenglobal: "0",
+
+  //Music Controller
+  buttonmusic: "0", //0 = off | 1 = on
+  buttonmusicmessage: "",
+  buttonmusichannel: "",
+  timechangemessage: "8000", //0 = disable | milisecond
+  openpublicbutton: "0", //0 = no | 1 = yes (for inside vc users)
+  forceusebutton: "0", //0 = no | 1 = yes (for everyone, include non-vc users)
+
+  //Playlist(s)
+  playlistuser: "",
+  playlistusercount: "1",
+  playlistuserpublic: "off",
+  playlistuserauto: "off",
+  playlistuserwords: "off",
+  playlistuserbypass: "0", //0 = off | 1 = on (bypass from blocking)
+  playlistcacheuser: "",
+  playlistcachecount: "1"
+
   Wallet: "1000", 
   XP: "100",
   Bank: "0",
